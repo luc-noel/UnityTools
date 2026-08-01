3a677beb5973706f78208992b06bb36c789013af
luc-noel <a.g.noel@live.ca>
2026-07-31 22:40:00 -0400

    Fix EasyReplaceFile not overriding file extensions

--- EasyReplaceFile.cs
+++ EasyReplaceFile.cs
@@ -28,29 +28,30 @@ public class EasyReplaceFile
         string selection = AssetDatabase.GetAssetPath(Selection.activeObject);
         // Get the extension so it can be passed into the editor window as the default extension filter
         string extension = Path.GetExtension(selection);
-        // Remove . in front of extension, otherwise the OpenFilePanel doubles up on it
-        extension = extension.Remove(0, 1);
 
-        // Opens the OS specific file browser 
-        string newFile = EditorUtility.OpenFilePanel("Replace File", "", extension);
+        // Opens the OS specific file browser
+        // Must remove . in front of extension, otherwise the OpenFilePanel doubles up on it
+        string newFile = EditorUtility.OpenFilePanel("Replace File", "", extension[1..]);
+        string newExtension = Path.GetExtension(newFile);
 
         // Make sure the newFile actually has a name/anything was selected
         if (newFile.Length > 0)
         {
-            ReplaceSelection(selection, newFile);
+            ReplaceSelection(selection, newFile, extension, newExtension);
+
             // Refresh the project assets so the file change is immediately visible in the Project files
             AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
         }
     }
 
     // Takes the path for the selected file and the one selected in the OS file browser and does the replacing
-    static void ReplaceSelection(string oldFile, string newFile)
+    static void ReplaceSelection(string oldFile, string newFile, string oldExtension, string newExtension)
     {
         // True by default, assume there will be no errors
         bool userContinue = true;
 
         // We only display a pop-up dialogue if RequestWarningMessage is true
-        if (RequestWarningMessage(oldFile, newFile, out string message))
+        if (RequestWarningMessage(oldFile, newFile, oldExtension, newExtension, out string message))
         {
             // Pop-up dialogue with our warning, user can continue or cancel the operation
             userContinue = EditorUtility.DisplayDialog("Replace File", message, "Continue", "Cancel");
@@ -58,30 +59,40 @@ public class EasyReplaceFile
         if (userContinue)
         {
             FileUtil.ReplaceFile(newFile, oldFile);
+
+            // FileUtil.ReplaceFile doesn't handle changing file extensions, must do it ourselves
+            if (oldExtension != newExtension)
+            {
+                string path = Path.ChangeExtension(oldFile, newExtension);
+                File.Move(oldFile, path);
+
+                string metaPath =  oldFile + ".meta";
+                string newMetaPath = path + ".meta";
+
+                File.Move(metaPath, newMetaPath);
+            }
         }
     }
 
     // Checks whether the incoming file has a name or file extension conflict and formats a warning message if so.
-    static bool RequestWarningMessage(string oldFile, string newFile, out string message)
+    static bool RequestWarningMessage(string oldFile, string newFile, string oldExtension, string newExtension, out string message)
     {
         string warning = string.Empty;
 
         string oldFileName = Path.GetFileNameWithoutExtension(oldFile);
         string newFileName = Path.GetFileNameWithoutExtension(newFile);
-        string oldFileExtension = Path.GetExtension(oldFile);
-        string newFileExtension = Path.GetExtension(newFile);
 
         if (oldFileName != newFileName)
         {
             warning += string.Format
             (
                 // u2022 is the Unicodefor a bullet character
-                "\u2022 File names \"{0}\" and \"{1}\" don't match.",
+                "\u2022 File names \"{0}\" and \"{1}\" don't match. The file name will be kept.",
                 oldFileName,
                 newFileName
             );
         }
-        if (oldFileExtension != newFileExtension)
+        if (oldExtension != newExtension)
         {
             // Add a newline between the file name and extension warning if both are true
             if (!string.IsNullOrEmpty(warning))
@@ -89,9 +100,9 @@ public class EasyReplaceFile
 
             warning += string.Format
             (
-                "\u2022 File extensions {0} and {1} don't match.",
-                oldFileExtension.ToUpper(),
-                newFileExtension.ToUpper()
+                "\u2022 File extensions {0} and {1} don't match. The file extension will be changed.",
+                oldExtension.ToUpper(),
+                newExtension.ToUpper()
             );
         }
 
